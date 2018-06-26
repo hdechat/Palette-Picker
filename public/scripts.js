91daefd8fc22ac4c5c4eddef2d4d772c985941d3
Helen Dechat <hdechat@gmail.com>
2018-06-25 22:18:03 -0600

    Add getRandomHexCode function to scripts

---
+++ public/scripts.js
@@ -0,0 +1,12 @@
+const getRandomHexCode = () => {
+  const hexChars = '0123456789ABCDEF';
+  let hex = '#';
+
+  while (hex.length < 7) {
+    const randomIndex = Math.floor(Math.random() * 16)
+
+    hex += hexChars[randomIndex];
+  }
+
+  return hex;
+}
