839cb920569c1b7b193f0a622c83e4a692344c52
Helen Dechat <hdechat@gmail.com>
2018-06-26 17:59:33 -0600

    Add save palette to project feature

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -45,8 +45,8 @@ function Palette() {
 }
 
 const appendPalettes = (newPalette) => {
-  $('.saved-palettes').append(`
-    <li id=${newPalette.id}>${newPalette.name}
+  $('.saved-palettes-list').append(`
+    <li class="palette" id=${newPalette.id}>${newPalette.name}
       <div class="saved-palette" style="background-color: ${newPalette.colors[0]}" ></div>
       <div class="saved-palette" style="background-color: ${newPalette.colors[1]}"></div>
       <div class="saved-palette" style="background-color: ${newPalette.colors[2]}"></div>
@@ -73,7 +73,7 @@ function Project() {
 }
 
 const appendProjects = (newProject) => {
-  $('.saved-projects').append(`<li id=${newProject.id}>${newProject.name}</li>`);
+  $('.saved-projects-list').append(`<ul class="project" id=${newProject.id}>${newProject.name}</ul>`);
 }
 
 const addToSavedProjects = () => {
@@ -92,3 +92,28 @@ const duplicateNameResponse = (name) => {
 }
 
 $('.save-project-button').on('click', addToSavedProjects);
+
+// save Palette to Project
+let selectedPalette;
+let selectedProject;
+
+function selectPalette() {
+  $(this).css('border', 'solid thin');
+  selectedPalette = $(this).clone();
+}
+
+function selectProject() {
+  $(this).css('border', 'solid thin');
+  selectedProject = this;
+}
+
+$('.saved-palettes-list').on('click', '.palette', selectPalette);
+$('.saved-projects-list').on('click', '.project', selectProject);
+
+
+const addPaletteToProject = () => {
+  console.log(selectedPalette)
+  $(selectedProject).append(selectedPalette)
+}
+
+$('.save-to-project-button').on('click', addPaletteToProject);
